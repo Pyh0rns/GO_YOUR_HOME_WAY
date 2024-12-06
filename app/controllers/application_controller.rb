b02c429e28b9ab1c941badd1739df14115f0d234
PierreYves Hoorens <pyhoorens@gmail.com>
2024-12-06 15:27:17 +0100

    changed after login path for user and company

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    if current_user
+      dashboard_path
+    elsif current_company
+      dashboard_pro_path
+    end
+  end
 end
