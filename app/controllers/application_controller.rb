3b5bbaa807f7e2f5b8c3b689008a804d92c081f7
py horns <pyhoorens@gmail.com>
2024-12-08 20:09:29 +0100

    fixed app when first time user with no property

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,18 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    if current_user
+    if current_user.properties.empty?
+      new_property_path
+    elsif current_user
       dashboard_path
     elsif current_company
       dashboard_pro_path
     end
   end
+
+  # def after_sign_up_path_for(resource)
+  #   raise
+  #     new_property_path
+  # end
+
 end
