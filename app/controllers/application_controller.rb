0c236d7d1f2ab94dd5ae3630c0e39829acbf5bc7
py horns <pyhoorens@gmail.com>
2024-12-09 17:48:55 +0100

    added fields to companies sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     if current_company
@@ -11,4 +12,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :phone_numer])
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :phone_numer])
+  end
 end
