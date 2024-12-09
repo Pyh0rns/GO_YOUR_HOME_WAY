d3e7672b23b959141059580ad3a5f3ac9411679a
py horns <pyhoorens@gmail.com>
2024-12-09 10:27:53 +0100

    carousel removed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-
-import { Application } from '@hotwired/stimulus'
-import Carousel from '@stimulus-components/carousel'
-
-const application = Application.start()
-application.register('carousel', Carousel)
