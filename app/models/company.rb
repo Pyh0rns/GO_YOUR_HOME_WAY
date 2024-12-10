97e9b3a0f38e9453c2a1fb8134e68b43a0803e0e
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-12-10 15:37:14 +0100

    seed fonctionnel / langue Fr sur les inputs /

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,11 @@
 class Company < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 
   has_many :work_actions, as: :actionable
   has_many :favorites
@@ -10,11 +13,13 @@ class Company < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   include PgSearch::Model
+
   # ajouter plus tard recherche par localisation et spécialité
   pg_search_scope :search,
-  against: [ :name ],
-  using: {
-    tsearch: { prefix: true }
-  }
-
+                  against: %i[name address],
+                  using: {
+                    tsearch: {
+                      prefix: true,
+                    },
+                  }
 end
