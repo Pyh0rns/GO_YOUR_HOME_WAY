4b995bcbf98693649ff118e29ea0d14f3c4fc5c0
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-12-12 14:54:00 +0100

    feat: bottom nav link

---
+++ app/javascript/controllers/navigation_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["navbar", "bottomNav", "sidebar"];
+
+  connect() {
+    console.log("naviation controller connected");
+
+    this.checkScreenSize();
+    window.addEventListener("resize", this.checkScreenSize.bind(this));
+  }
+
+  checkScreenSize() {
+    const isMobile = window.innerWidth < 768;
+
+    console.log(isMobile); // Breakpoint mobile
+
+    this.navbarTarget.classList.toggle("d-none", isMobile);
+    this.bottomNavTarget.classList.toggle("d-none", !isMobile);
+    this.sidebarTarget.classList.toggle("d-none", isMobile);
+  }
+}
